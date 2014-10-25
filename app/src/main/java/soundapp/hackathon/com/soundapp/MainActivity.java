a6031998fdb686b80225bcc601a251b1298468c3
njzk2 <masperos+github@gmail.com>
2014-10-25 13:24:11 -0400

    first attempt with audio record

--- app/src/main/java/soundapp/hackathon/com/soundapp/MainActivity.java
+++ app/src/main/java/soundapp/hackathon/com/soundapp/MainActivity.java
@@ -1,23 +1,69 @@
 package soundapp.hackathon.com.soundapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
+    boolean recording = false;
+    AudioRecord audioRecord;
+    String mFileName = Environment.getExternalStorageDirectory().getAbsolutePath() + "/audiorecordtest.3gp";
+    int bufSize = 44100 * 2 * 1 * 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //mFileName = getFilesDir().getAbsolutePath() + "/test.wav";
         setContentView(R.layout.activity_main);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                stub.findViewById(R.id.record).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!recording) {
+                            audioRecord = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, 44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufSize);
+
+                            audioRecord.startRecording();
+                            recording = true;
+                            ((TextView) view).setText(R.string.stop);
+                        } else {
+                            ((TextView) view).setText(R.string.record);
+
+                            byte[] buf = new byte[bufSize];
+                            audioRecord.read(buf, 0, bufSize);
+                            audioRecord.stop();
+                            try {
+                                new FileOutputStream(mFileName).write(buf, 0, bufSize);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            MediaPlayer player = new MediaPlayer();
+                            try {
+                                player.setDataSource(mFileName);
+                                player.start();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            recording = false;
+                        }
+                    }
+                });
             }
         });
     }
