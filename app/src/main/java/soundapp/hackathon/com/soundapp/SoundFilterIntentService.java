b3c046e59a9ab6d936c9fcda298254081dae0cfc
John Gosset <john.gosset@gmail.com>
2014-10-25 16:40:47 -0400

    uncomment SoundFilterIntentService stuff

--- app/src/main/java/soundapp/hackathon/com/soundapp/SoundFilterIntentService.java
+++ app/src/main/java/soundapp/hackathon/com/soundapp/SoundFilterIntentService.java
@@ -7,47 +7,47 @@ import android.os.IBinder;
 /**
  * Created by john on 25/10/14.
  */
-public class SoundFilterIntentService {
-//    public SoundFilterIntentService(String name) {
-//        super(name);
-//    }
-//
-//    public SoundFilterIntentService() {
-//        super("SoundFilterIntentService");
-//    }
-//
-//    @Override
-//    public void setIntentRedelivery(boolean enabled) {
-//        super.setIntentRedelivery(enabled);
-//    }
-//
-//    @Override
-//    public void onCreate() {
-//        super.onCreate();
-//    }
-//
-//    @Override
-//    public void onStart(Intent intent, int startId) {
-//        super.onStart(intent, startId);
-//    }
-//
-//    @Override
-//    public int onStartCommand(Intent intent, int flags, int startId) {
-//        return super.onStartCommand(intent, flags, startId);
-//    }
-//
-//    @Override
-//    public void onDestroy() {
-//        super.onDestroy();
-//    }
-//
-//    @Override
-//    public IBinder onBind(Intent intent) {
-//        return super.onBind(intent);
-//    }
-//
-//    @Override
-//    protected void onHandleIntent(Intent intent) {
-//
-//    }
+public class SoundFilterIntentService extends IntentService {
+    public SoundFilterIntentService(String name) {
+        super(name);
+    }
+
+    public SoundFilterIntentService() {
+        super("SoundFilterIntentService");
+    }
+
+    @Override
+    public void setIntentRedelivery(boolean enabled) {
+        super.setIntentRedelivery(enabled);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return super.onBind(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
 }
